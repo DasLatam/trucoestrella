e38a30f1f48b0ae801e54615961a4eb6d5bb408d
DAS Latam <daslatamarg@gmail.com>
2025-08-08 18:57:49 +0000

    fix: 2v2

--- frontend/src/GameScreen.js
+++ frontend/src/GameScreen.js
@@ -192,6 +192,7 @@ function GameScreen() {
 
     return (
         <div className="w-full h-screen bg-truco-green flex overflow-hidden">
+            {/* Área de Juego Principal */}
             <div className="flex-grow relative p-4 flex flex-col">
                 <div className="absolute top-4 left-4 z-20">
                     <div className="bg-black bg-opacity-50 p-3 rounded-lg text-white text-base w-56">
@@ -201,21 +202,35 @@ function GameScreen() {
                     <Link to="/" className="bg-red-600 text-white font-bold py-2 px-4 rounded-md mt-4 inline-block">Abandonar</Link>
                 </div>
 
+                {/* **LA CORRECCIÓN: Se vuelve a centrar al oponente en la parte superior.** */}
                 {otherPlayers.map((player) => (
                     <PlayerUI key={player.id} player={player} cardsCount={gameState.hands[player.id]?.length || 0} position={playerPositions[player.id]} isTurn={gameState.turn === player.id} />
                 ))}
 
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[65vw] h-[55vh] bg-truco-brown rounded-[50%] border-8 border-yellow-800 shadow-2xl flex justify-around items-center px-10">
-                    {[1, 2, 3].map(roundNum => (
-                        <div key={roundNum} className="flex flex-col justify-between h-full py-10">
-                            <div>
-                                {playedCardsByRound[roundNum].find(c => c.playedBy !== user.id) && <Card card={playedCardsByRound[roundNum].find(c => c.playedBy !== user.id)} />}
-                            </div>
-                            <div>
-                                {playedCardsByRound[roundNum].find(c => c.playedBy === user.id) && <Card card={playedCardsByRound[roundNum].find(c => c.playedBy === user.id)} />}
-                            </div>
+                {/* Mesa Ovalada */}
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[65vw] h-[55vh] bg-truco-brown rounded-[50%] border-8 border-yellow-800 shadow-2xl">
+                    {/* **DISEÑO MEJORADO: Slots fijos para las rondas con posicionamiento absoluto** */}
+                    {/* Slot Ronda 1 (Izquierda) */}
+                    <div className="absolute top-1/2 -translate-y-1/2 left-[25%] -translate-x-1/2">
+                        <div className="flex flex-col items-center space-y-5">
+                            {playedCardsByRound[1].find(c => c.playedBy !== user.id) && <Card card={playedCardsByRound[1].find(c => c.playedBy !== user.id)} />}
+                            {playedCardsByRound[1].find(c => c.playedBy === user.id) && <Card card={playedCardsByRound[1].find(c => c.playedBy === user.id)} />}
                         </div>
-                    ))}
+                    </div>
+                    {/* Slot Ronda 2 (Centro) */}
+                    <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+                        <div className="flex flex-col items-center space-y-5">
+                            {playedCardsByRound[2].find(c => c.playedBy !== user.id) && <Card card={playedCardsByRound[2].find(c => c.playedBy !== user.id)} />}
+                            {playedCardsByRound[2].find(c => c.playedBy === user.id) && <Card card={playedCardsByRound[2].find(c => c.playedBy === user.id)} />}
+                        </div>
+                    </div>
+                    {/* Slot Ronda 3 (Derecha) */}
+                    <div className="absolute top-1/2 -translate-y-1/2 left-[75%] -translate-x-1/2">
+                         <div className="flex flex-col items-center space-y-5">
+                            {playedCardsByRound[3].find(c => c.playedBy !== user.id) && <Card card={playedCardsByRound[3].find(c => c.playedBy !== user.id)} />}
+                            {playedCardsByRound[3].find(c => c.playedBy === user.id) && <Card card={playedCardsByRound[3].find(c => c.playedBy === user.id)} />}
+                        </div>
+                    </div>
                 </div>
                 
                 {isMyTurnToRespond && <ChantNotification trucoState={gameState.truco} onResponse={handleResponse} />}
